3e2b2d26fbcb0c5db0d0d1a531e27e524669176e
Kamrobek541 <an4ous541@gmail.com>
2026-01-21 00:37:38 +0100

    Mini app edit port

--- server.py
+++ server.py
@@ -363,4 +363,4 @@ async def cryptapi_webhook(request: Request):
 app.mount("/", StaticFiles(directory="webapp", html=True), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
